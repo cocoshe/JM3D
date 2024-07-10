14a15ace9bd502a2e4fd110df327ceae5457db00
coco <1228759711@qq.com>
2024-07-10 21:29:51 +0000

    add eval script

--- JM3D-LLM/scripts/eval.sh
+++ JM3D-LLM/scripts/eval.sh
@@ -1,7 +1,8 @@
-MODEL_PATH='checkpoints/yk_test_ckpt_stage2'
+# MODEL_PATH='checkpoints/yk_test_ckpt_stage2'
+MODEL_PATH='checkpoints/yk_test_ckpt_stage3'
 # export MODEL_PATH=/home/myw/wuchangli/PointLLM/checkpoints/PointLLM_7B_v1.1
 export RES_PATH=$MODEL_PATH/evaluation/PointLLM_brief_description_val_200_GT_Objaverse_captioning_prompt2.json
 
-# python llava/eval/eval_objaverse.py --model_name $MODEL_PATH --task_type captioning --prompt_index 2 --use_color
+python llava/eval/eval_objaverse.py --model_name $MODEL_PATH --task_type captioning --prompt_index 2 --use_color
 
 python llava/eval/traditional_evaluator.py --results_path $RES_PATH
